dffb62122884b4eed28acc6f66fd4d4fa34ba1b1
alexRazvan <alexrvan@gmail.com>
2019-12-05 20:51:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65c48301044426abc694c7c1f7d78b1c.js"
+  "/precache-manifest.373b1527ab9720ca1e36f2f2b12e4d49.js"
 );
 
 self.addEventListener('message', (event) => {
